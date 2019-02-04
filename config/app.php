5e5c4a2b5b5f9e8bf02b6fc5ed9830fb38ac2011
Etudiant <=>
2019-02-04 10:56:12 +0100

    mvc template :tada:

---
+++ config/app.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Aliases : raccourcis pour les noms de classes
+ */
+class_alias('\Bramus\Router\Router', 'Router');
+/**
+ * Constantes : éléments de configuration propres au système
+ */
+const WEBSITE_TITLE = "HP Game en MVC";
+const BASE_URL = "localhost/hp-games";
+/**
+ * Liste des dossiers source pour l'autoload des classes
+ */
+const CLASSES_SOURCES = [
+    'controllers',
+    'config',
+    'models',
+];
